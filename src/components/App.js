e28d2eaceb76ae2d3a882c78f6bbfac8aa5c9305
anitha-anii <anithachowdary355@gmail.com>
2023-07-19 15:39:11 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+    const[search,setSearch] =useState('');
+    const[list,setList] =useState([]);
+
+  useEffect(()=>{
+    let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
+    let filteredFruits = fruits.filter(fruit => fruit.includes(search.toLowerCase()));
+    setList(filteredFruits);
+  },[search])
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        <ul>
+          {list.map((item,index)=>(
+          <li key={index}>{item}</li>
+          ))}
+        </ul>
     </div>
   )
 }
